fe31503117bc3f1522ca951db0cab7f6861a010b
Curtis J Pierce <Curtisjpierce39@gmail.com>
2025-03-10 08:51:52 -0700

    add initial description

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,3 +9,15 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+=======
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+>>>>>>> 8a43777 (add initial description)
